136ac41f6c34f7bf407bd858f1c923608fc5fa54
Patrick Allen <prallen90@gmail.com>
2017-03-21 00:50:22 -0400

    Chance tabs to spaces....

--- src/cpu.rs
+++ src/cpu.rs
@@ -15,191 +15,190 @@ pub struct CPU {
 
 
 impl CPU {
-	pub fn new() -> CPU {
-		CPU {
-			mem: [0; 4096],
-			regs: [0; 16],
-			index: 0,
-			stack: [0; 16],
-			sp: 0, // Pointer to the topmost of the stack
-			opcode: 0,
-			pc: 0x200
-		}
-	}
-	pub fn cycle(&mut self) {
-		let pim = self.pc as usize;
-		let opcode = self.opcode_at_address(pim).to_owned();
-		self.pc += 2;
-		self.run_operation_for_opcode(opcode);
-		self.opcode = opcode;
+    pub fn new() -> CPU {
+        CPU {
+            mem: [0; 4096],
+            regs: [0; 16],
+            index: 0,
+            stack: [0; 16],
+            sp: 0, // Pointer to the topmost of the stack
+            opcode: 0,
+            pc: 0x200
+        }
+    }
+    pub fn cycle(&mut self) {
+        let pim = self.pc as usize;
+        let opcode = self.opcode_at_address(pim).to_owned();
+        self.pc += 2;
+        self.run_operation_for_opcode(opcode);
+        self.opcode = opcode;
 
-		println!("Stack Pointer: {}", self.sp);
-		println!("Top of stack: {}", self.stack[self.sp as usize]);
-	}
-	pub fn load_rom(&mut self, filepath: &str) {
-		let mut rom: Vec<u8> = Vec::new();
-		let mut file = File::open(filepath).unwrap();
-		file.read_to_end(&mut rom);
+        // println!("Stack Pointer: {}", self.sp);
+        // println!("Top of stack: {}", self.stack[self.sp as usize]);
+    }
+    pub fn load_rom(&mut self, filepath: &str) {
+        let mut rom: Vec<u8> = Vec::new();
+        let mut file = File::open(filepath).unwrap();
+        file.read_to_end(&mut rom);
 
-		for (i, mut byte) in rom.iter().enumerate() {
-			self.mem[i + 512] = *byte;
-		}
-	}
-	pub fn opcode_at_address(&self, address: usize) -> u16 {
-		let mut ret = self.mem[address] as u16;
-		let ret2 = self.mem[address + 1] as u16;
-		(ret << 8 | ret2)
-	}
-	pub fn run_operation_for_opcode<'a>(&mut self, code: u16) {
-		let rex = match code & 0xF000 {
-			0x0000 => self.x0_refine_code(code),
-			0x1000 => self.x1_goto(code),
-			0x2000 => self.x2_call_sub(code),
-			0x3000 => self.x3_skip_if_eq(code),
-			0x4000 => self.x4_skip_if_neq(code),
-			0x5000 => self.x5_skip_if_regs_eq(code),
-			0x6000 => self.x6_refine_code(code),
-			0x7000 => self.x7_add_to_reg(code),
-			0x8000 => self.x8_refine_code(code),
-			0x9000 => self.x9_skip_if_regs_neq(code),
-			0xA000 => self.xa_store_in_i(code),
-			0xB000 => self.xb_jump_to(code),
-			0xC000 => self.xc_set_reg_random(code),
-			0xD000 => self.xd_draw_sprite(code),
-			0xE000 => self.xe_refine_code(code),
-			0xF000 => self.xf_refine_code(code),
-			_ => panic!("run_operation failed."),
-		};
-	}
-	fn x0_refine_code(&mut self, code: u16) {
-		// Could be 0x00E0, 0x00EE, or 0x0NNN
-		match code {
-			0x00EE 	=> self.x0_return_from_sub(code),
-			// 0x00E0 	=> "0x00E0",
-			// _ 		=> "0x0nnn",
-			_ => panic!("Nope")
-		}
-	}
-	fn x0_return_from_sub(&mut self, code: u16) {
-		self.pc = self.stack[self.sp as usize];
-		self.sp -=1;
-	}
-	fn x1_goto(&mut self, code: u16) {
-		// GOTO -> Set the PC to the specified address.
-		// Doing so will make the interpreter pick up at this address
-		// on the next cycle.
-		self.pc = code & 0x0FFF;
-	}
-	fn x2_call_sub(&mut self, code: u16) {
-		self.sp += 1;
-		self.stack[self.sp as usize] = self.pc;
-		println!("Result: {}", code & 0xFFF);
-		self.pc = code & 0x0FFF;
-	}
-	fn x3_skip_if_eq(&mut self, code: u16) {
-		// "0x3xnn"
-	}
-	fn x4_skip_if_neq(&mut self, code: u16) {
-		// "0x4xnn"
-	}
-	fn x5_skip_if_regs_eq(&mut self, code: u16) {
-		// "0x5xy0"
-	}
-	fn x6_refine_code(&mut self, code: u16) {
-		// "0x6xnn"
-	}
-	fn x7_add_to_reg(&mut self, code: u16) {
-		// "0x7xnn"
-	}
-	fn x8_refine_code(&mut self, code: u16) {
-		// 0x8xy0, 0x8xy1, 0x8xy2, 0x8xy3, 0x8xy4, 0x8xy5, 0x8xy6, 0x8xy7, 0x8xyE
-		// match code & 0x000F {
-		// 	0x0 => "0x8xy0",
-		// 	0x1 => "0x8xy1",
-		// 	0x2 => "0x8xy2",
-		// 	0x3 => "0x8xy3",
-		// 	0x4 => "0x8xy4",
-		// 	0x5 => "0x8xy5",
-		// 	0x6 => "0x8xy6",
-		// 	0x7 => "0x8xy7",
-		// 	0xE => "0x8xyE",
-		// 	_ => "Error"
-		// }
-	}
-	fn x9_skip_if_regs_neq(&mut self, code: u16) {
-		// "0x9xy0"
-	}
-	fn xa_store_in_i(&mut self, code: u16) {
-		// "0xAnnn"
-	}
-	fn xb_jump_to(&mut self, code: u16) {
-		// "0xBnnn"
-	}
-	fn xc_set_reg_random(&mut self, code: u16) {
-		// "0xCxnn"
-	}
-	fn xd_draw_sprite(&mut self, code: u16) {
-		// "0xDxyn"
-	}
-	fn xe_refine_code(&mut self, code: u16) {
-		// // 0xEx9E, 0xExA1
-		// match code & 0x00FF {
-		// 	0x9E => "0xEx9E",
-		// 	0xA1 => "0xExA1",
-		// 	_ => "Error"
-		// }
-	}
-	fn xf_refine_code(&mut self, code: u16) {
-		// // 0xFx07, 0xFx0A, 0xFx15, 0xFx18, 0xFx1E, 0xFx29, 0xFx33, 0xFx55, 0xFx65
-		// match code & 0x00FF {
-		// 	0x07 => "0xFx07",
-		// 	0x0A => "0xFx0A",
-		// 	0x15 => "0xFx15",
-		// 	0x18 => "0xFx18",
-		// 	0x1E => "0xFx1E",
-		// 	0x29 => "0xFx29",
-		// 	0x33 => "0xFx33",
-		// 	0x55 => "0xFx55",
-		// 	0x65 => "0xFx65",
-		// 	_ => "Error"
-		// }
-	}
+        for (i, mut byte) in rom.iter().enumerate() {
+            self.mem[i + 512] = *byte;
+        }
+    }
+    pub fn opcode_at_address(&self, address: usize) -> u16 {
+        let mut ret = self.mem[address] as u16;
+        let ret2 = self.mem[address + 1] as u16;
+        (ret << 8 | ret2)
+    }
+    pub fn run_operation_for_opcode<'a>(&mut self, code: u16) {
+        let rex = match code & 0xF000 {
+            0x0000 => self.x0_refine_code(code),
+            0x1000 => self.x1_goto(code),
+            0x2000 => self.x2_call_sub(code),
+            0x3000 => self.x3_skip_if_eq(code),
+            0x4000 => self.x4_skip_if_neq(code),
+            0x5000 => self.x5_skip_if_regs_eq(code),
+            0x6000 => self.x6_refine_code(code),
+            0x7000 => self.x7_add_to_reg(code),
+            0x8000 => self.x8_refine_code(code),
+            0x9000 => self.x9_skip_if_regs_neq(code),
+            0xA000 => self.xa_store_in_i(code),
+            0xB000 => self.xb_jump_to(code),
+            0xC000 => self.xc_set_reg_random(code),
+            0xD000 => self.xd_draw_sprite(code),
+            0xE000 => self.xe_refine_code(code),
+            0xF000 => self.xf_refine_code(code),
+            _ => panic!("run_operation failed."),
+        };
+    }
+    fn x0_refine_code(&mut self, code: u16) {
+        // Could be 0x00E0, 0x00EE, or 0x0NNN
+        match code {
+            0x00EE  => self.x0_return_from_sub(code),
+            // 0x00E0   => "0x00E0",
+            // _        => "0x0nnn",
+            _ => panic!("Nope")
+        }
+    }
+    fn x0_return_from_sub(&mut self, code: u16) {
+        self.pc = self.stack[self.sp as usize];
+        self.sp -=1;
+    }
+    fn x1_goto(&mut self, code: u16) {
+        // GOTO -> Set the PC to the specified address.
+        // Doing so will make the interpreter pick up at this address
+        // on the next cycle.
+        self.pc = code & 0x0FFF;
+    }
+    fn x2_call_sub(&mut self, code: u16) {
+        self.sp += 1;
+        self.stack[self.sp as usize] = self.pc;
+        self.pc = code & 0x0FFF;
+    }
+    fn x3_skip_if_eq(&mut self, code: u16) {
+        // "0x3xnn"
+    }
+    fn x4_skip_if_neq(&mut self, code: u16) {
+        // "0x4xnn"
+    }
+    fn x5_skip_if_regs_eq(&mut self, code: u16) {
+        // "0x5xy0"
+    }
+    fn x6_refine_code(&mut self, code: u16) {
+        // "0x6xnn"
+    }
+    fn x7_add_to_reg(&mut self, code: u16) {
+        // "0x7xnn"
+    }
+    fn x8_refine_code(&mut self, code: u16) {
+        // 0x8xy0, 0x8xy1, 0x8xy2, 0x8xy3, 0x8xy4, 0x8xy5, 0x8xy6, 0x8xy7, 0x8xyE
+        // match code & 0x000F {
+        //  0x0 => "0x8xy0",
+        //  0x1 => "0x8xy1",
+        //  0x2 => "0x8xy2",
+        //  0x3 => "0x8xy3",
+        //  0x4 => "0x8xy4",
+        //  0x5 => "0x8xy5",
+        //  0x6 => "0x8xy6",
+        //  0x7 => "0x8xy7",
+        //  0xE => "0x8xyE",
+        //  _ => "Error"
+        // }
+    }
+    fn x9_skip_if_regs_neq(&mut self, code: u16) {
+        // "0x9xy0"
+    }
+    fn xa_store_in_i(&mut self, code: u16) {
+        // "0xAnnn"
+    }
+    fn xb_jump_to(&mut self, code: u16) {
+        // "0xBnnn"
+    }
+    fn xc_set_reg_random(&mut self, code: u16) {
+        // "0xCxnn"
+    }
+    fn xd_draw_sprite(&mut self, code: u16) {
+        // "0xDxyn"
+    }
+    fn xe_refine_code(&mut self, code: u16) {
+        // // 0xEx9E, 0xExA1
+        // match code & 0x00FF {
+        //  0x9E => "0xEx9E",
+        //  0xA1 => "0xExA1",
+        //  _ => "Error"
+        // }
+    }
+    fn xf_refine_code(&mut self, code: u16) {
+        // // 0xFx07, 0xFx0A, 0xFx15, 0xFx18, 0xFx1E, 0xFx29, 0xFx33, 0xFx55, 0xFx65
+        // match code & 0x00FF {
+        //  0x07 => "0xFx07",
+        //  0x0A => "0xFx0A",
+        //  0x15 => "0xFx15",
+        //  0x18 => "0xFx18",
+        //  0x1E => "0xFx1E",
+        //  0x29 => "0xFx29",
+        //  0x33 => "0xFx33",
+        //  0x55 => "0xFx55",
+        //  0x65 => "0xFx65",
+        //  _ => "Error"
+        // }
+    }
 }
 
 #[test]
 pub fn test_run_operation_for_goto() {
-	// opcode = 0x10 << 8 | 0xF0 = 0x10F0
-	// this means that our GOTO should take the last 3 hex
-	// digits and put them into our program counter
-	let mut cpu = CPU::new();
-	cpu.mem[0x200] = 0x10;
-	cpu.mem[0x201] = 0xF0;
-	cpu.cycle();
-	assert_eq!(0x0F0, cpu.pc);
+    // opcode = 0x10 << 8 | 0xF0 = 0x10F0
+    // this means that our GOTO should take the last 3 hex
+    // digits and put them into our program counter
+    let mut cpu = CPU::new();
+    cpu.mem[0x200] = 0x10;
+    cpu.mem[0x201] = 0xF0;
+    cpu.cycle();
+    assert_eq!(0x0F0, cpu.pc);
 }
 
 #[test]
 pub fn test_run_operation_for_call_sub() {
-	let mut cpu = CPU::new();
-	cpu.mem[0x200] = 0x21;
-	cpu.mem[0x201] = 0x00;
-	cpu.cycle();
-	assert_eq!(cpu.sp, 1);
-	assert_eq!(cpu.stack[cpu.sp as usize], 0x202);
-	assert_eq!(cpu.pc, 0x100);
+    let mut cpu = CPU::new();
+    cpu.mem[0x200] = 0x21;
+    cpu.mem[0x201] = 0x00;
+    cpu.cycle();
+    assert_eq!(cpu.sp, 1);
+    assert_eq!(cpu.stack[cpu.sp as usize], 0x202);
+    assert_eq!(cpu.pc, 0x100);
 }
 
 #[test]
 pub fn test_return_from_sub() {
-	let mut cpu = CPU::new();
-	cpu.mem[0x200] = 0x23;
-	cpu.mem[0x201] = 0x00;
-	cpu.mem[0x300] = 0x00;
-	cpu.mem[0x301] = 0xEE;
-	cpu.cycle();
-	cpu.cycle();
-	assert_eq!(cpu.pc, 0x202);
-	assert_eq!(cpu.sp, 0);
+    let mut cpu = CPU::new();
+    cpu.mem[0x200] = 0x23;
+    cpu.mem[0x201] = 0x00;
+    cpu.mem[0x300] = 0x00;
+    cpu.mem[0x301] = 0xEE;
+    cpu.cycle();
+    cpu.cycle();
+    assert_eq!(cpu.pc, 0x202);
+    assert_eq!(cpu.sp, 0);
 }
 
 pub fn parse_opcode(code: u16) -> Result<u16, &'static str> {
