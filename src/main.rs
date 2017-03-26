4d339fd396109642ceb1d1912a2bfdc7a802dd53
Patrick Allen <prallen90@gmail.com>
2017-03-26 16:57:15 -0400

    We can now pass the rom title via the command line.

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,20 @@ mod display;
 mod keyboard;
 mod opcodes;
 
-
+use std::env;
+use std::path::Path;
 use cpu::CPU;
 
 pub const DEBUG: bool = true;
-
+pub const DEBUG_CHUNK: u16 = 8;
+pub const DO_CHUNK_DEBUG: bool = false;
 
 fn main() {
-	let connect4 = "/Users/patallen/Code/Rust/chip8/src/roms/15PUZZLE.ch8";
-	let mut cpu = CPU::new();
-	cpu.load_rom(connect4);
-	// let mut bugger = Debugger::new(connect4);
-	// bugger.run();
-	cpu.run();
+    let path = Path::new("./roms/");
+    let rom = env::args().nth(1).unwrap() + ".ch8";
+    let rom_path = path.join(rom);
+
+    let mut cpu = CPU::new();
+    cpu.load_rom(&*rom_path.to_string_lossy());
+    cpu.run();
 }
